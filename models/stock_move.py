bbedbd2e72b87eb57a5ed7f56ecf5448d7922857
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-25 23:29:23 -0600

    Up

--- models/stock_move.py
+++ models/stock_move.py
@@ -8,3 +8,12 @@ class StockMove(models.Model):
     tipo = fields.Char(string="Tipo")
     kilos = fields.Float(string="Kilos")
     planta = fields.Char(string="Planta")
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    gramaje = fields.Float(string="Gramaje")
+    ancho = fields.Float(string="Ancho")
+    tipo = fields.Char(string="Tipo")
+    kilos = fields.Float(string="Kilos")
+    planta = fields.Char(string="Planta")
