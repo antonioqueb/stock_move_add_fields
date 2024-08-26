4beb176043cb5775e2e1a887b1d5b6885829f5e7
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-26 00:00:12 -0600

    Up

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,14 +1,5 @@
 from odoo import fields, models
 
-class StockMove(models.Model):
-    _inherit = 'stock.move'
-
-    gramaje = fields.Float(string="Gramaje")
-    ancho = fields.Float(string="Ancho")
-    tipo = fields.Char(string="Tipo")
-    kilos = fields.Float(string="Kilos")
-    planta = fields.Char(string="Planta")
-
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
