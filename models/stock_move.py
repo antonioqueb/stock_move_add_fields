80c34b7fbfe832c6ac89a6db03e2c99935b21277
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-07 21:47:49 -0600

    Up

--- models/stock_move.py
+++ models/stock_move.py
@@ -14,43 +14,8 @@ class StockMove(models.Model):
 
     def _prepare_merge_move_distinct_fields(self):
         fields = super()._prepare_merge_move_distinct_fields()
-        return fields + ['gramaje', 'ancho', 'tipo', 'kilos', 'planta']
-
-    def _merge_moves(self, merge_into=None):
-        _logger.info("Iniciando agrupación de movimientos (_merge_moves). merge_into: %s", merge_into.id if merge_into else "None")
-        grouped_moves = self.env['stock.move']
-
-        for move in self:
-            key = (move.product_id.id, move.gramaje, move.ancho, move.tipo, move.kilos, move.planta)
-            _logger.info("Clave generada para movimiento ID %s: %s", move.id, key)
-
-            existing_move = grouped_moves.filtered(lambda m:
-                m.product_id.id == move.product_id.id and
-                m.gramaje == move.gramaje and
-                m.ancho == move.ancho and
-                m.tipo == move.tipo and
-                m.kilos == move.kilos and
-                m.planta == move.planta)
-
-            if existing_move:
-                for move_line in move.move_line_ids:
-                    existing_line = existing_move.move_line_ids.filtered(lambda l:
-                        l.product_id == move_line.product_id and
-                        l.lot_id == move_line.lot_id and
-                        l.gramaje == move_line.gramaje and
-                        l.ancho == move_line.ancho and
-                        l.tipo == move_line.tipo and
-                        l.kilos == move_line.kilos and
-                        l.planta == move_line.planta)
-
-                    if existing_line:
-                        existing_line.qty_done += move_line.qty_done
-                    else:
-                        move_line.copy(default={'move_id': existing_move.id})
-            else:
-                grouped_moves |= move
-
-        return grouped_moves
+        custom_fields = ['gramaje', 'ancho', 'tipo', 'kilos', 'planta']
+        return fields + custom_fields
 
     def _action_done(self, cancel_backorder=False):
         _logger.info("Iniciando validación de movimientos (_action_done). Movimientos a procesar: %s", self.ids)
@@ -63,7 +28,6 @@ class StockMove(models.Model):
         return res
 
     def _do_not_group_custom_fields(self):
-        # Si no necesitas más funcionalidad específica, puedes eliminar este método por completo.
         pass
 
     def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
@@ -76,6 +40,33 @@ class StockMove(models.Model):
             'planta': self.planta,
         })
         return vals
+
+    def _merge_moves(self, merge_into=False):
+        _logger.info("Iniciando agrupación de movimientos (_merge_moves). merge_into: %s", merge_into)
+
+        moves_by_key = {}
+        grouped_moves = self.env['stock.move']
+
+        for move in self:
+            key = (move.product_id.id, move.gramaje, move.ancho, move.tipo, move.kilos, move.planta)
+
+            if key in moves_by_key:
+                existing_move = moves_by_key[key]
+                for line in move.move_line_ids:
+                    existing_line = existing_move.move_line_ids.filtered(lambda l: l.product_id == move.product_id and l.lot_id == line.lot_id and
+                                                                         l.gramaje == line.gramaje and l.ancho == line.ancho and l.tipo == line.tipo and
+                                                                         l.kilos == line.kilos and l.planta == line.planta)
+                    if existing_line:
+                        existing_line.qty_done += line.qty_done
+                    else:
+                        line.copy({'move_id': existing_move.id})
+                move.state = 'cancel'
+            else:
+                moves_by_key[key] = move
+                grouped_moves |= move
+
+        _logger.info("Finalizando agrupación de movimientos. Movimientos agrupados resultantes: %s", grouped_moves.ids)
+        return grouped_moves
 
 
 class StockMoveLine(models.Model):
